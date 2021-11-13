8d0b96051e76eb89e0d49e0ba9c88e27b0fd9f29
Ron Yang <siyinyang@gmail.com>
2021-11-12 20:50:36 -0800

    add python setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Harmonic SDK",
+    version="0.0.1",
+    author="Ronald Yang",
+    author_email="ronald@harmonic.ai",
+    description="python SDK for Harmonic Consumer APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harmonicai/consumer_api_sdk.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
