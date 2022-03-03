4664de7fdf3cae0d09ceaa82ac7c169c4cd951c4
Ron Yang <siyinyang@gmail.com>
2022-03-02 22:13:13 -0800

    update(version) bump python sdk version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Harmonic SDK",
-    version="0.0.1",
+    version="0.0.2",
     author="Ronald Yang",
     author_email="ronald@harmonic.ai",
     description="python SDK for Harmonic Consumer APIs",
